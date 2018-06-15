44cdfff8bcd44be73e61108231099d8d487f22f0
JackDenson <jdenson2019@student.amsacs.org>
2018-06-15 13:58:15 -0400

    added php for new question

---
+++ htdocs/php/NewQuestion.php
@@ -0,0 +1,9 @@
+<?php 
+	if(isset($_POST['submit'])){
+		$newQuestion = $_POST['newQuestionName']
+		mysql_query("INSERT INTO QUESTION(question_id, category_id, input_type_id, statement) VALUES((SELECT 1+MAX(question_id) FROM QUESTION), 1, 1, ".$newQuestion.")")
+	}
+	<script type="text/javascript">
+		window.location = "../TeacherStrategies.html";
+	</script> 
+	?>
