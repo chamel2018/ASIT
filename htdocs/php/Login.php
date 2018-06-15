a07c1cd2519e2e7c383b04aad9f29409c9b98b90
rhennessey2019 <39124735+rhennessey2019@users.noreply.github.com>
2018-06-15 14:26:04 -0400

    continued work on SQL queries

--- htdocs/php/Login.php
+++ htdocs/php/Login.php
@@ -1,27 +1,26 @@
 <html>
 <body>
 
-Username: <?php 
+Username: <?php
 $uname = $_REQUEST["Username"];
 echo 'bleh';
-echo $uname; 
+echo $uname;
 ?><br>
 
 Password hash: <?php if (($_REQUEST["Password"]=='')) {
 	echo 'Password cannot be blank';
 }else{
-	mysql_connect('localhost','root','');
-    mysql_select_db("test");
-	$pwhash = hash('sha256', $_REQUEST["Password"])
+		$mysqli = new mysqli("localhost", "root", "", "test");
+	$pwhash = hash('sha256', $_REQUEST["Password"]);
 	echo $pwhash;
-	if(mysql_query("SELECT passwordhash FROM PERSON WHERE username = ".$uname) == $pwhash){
+	if($mysqli->query("SELECT passwordhash FROM PERSON WHERE username = ".$uname) == $pwhash){
 		$cookievalue = random_bytes(32);
 		setcookie("user", $cookievalue, time()+3600, "/");
-		UPDATE PERSON (active_cookie) VALUES ($cookievalue) WHERE username = $uname; 
+		$mysqli->query("UPDATE PERSON (active_cookie) VALUES ($cookievalue) WHERE username = $uname ".$uname);
 	}else{
 		echo 'error!';
 	}
-} 
+}
 /*sanitize this for sql injection, also we must add a connection to the database at the beginning once we have a database.*/
 ?>
 </body>
