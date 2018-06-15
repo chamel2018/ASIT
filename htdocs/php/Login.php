4ec338ae93c151fd9c4b164d49e5fc46719bb39b
rhennessey2019 <39124735+rhennessey2019@users.noreply.github.com>
2018-06-15 13:44:32 -0400

    no idea what this is

--- htdocs/php/Login.php
+++ htdocs/php/Login.php
@@ -1,11 +1,28 @@
 <html>
 <body>
 
-Username: <?php echo $_REQUEST["Username"]; ?><br>
+Username: <?php 
+$uname = $_REQUEST["Username"];
+echo 'bleh';
+echo $uname; 
+?><br>
+
 Password hash: <?php if (($_REQUEST["Password"]=='')) {
 	echo 'Password cannot be blank';
 }else{
-	echo hash('sha256', $_REQUEST["Password"]);
-} ?>
+	mysql_connect('localhost','root','');
+    mysql_select_db("test");
+	$pwhash = hash('sha256', $_REQUEST["Password"])
+	echo $pwhash;
+	if(mysql_query("SELECT passwordhash FROM PERSON WHERE username = ".$uname) == $pwhash){
+		$cookievalue = random_bytes(32);
+		setcookie("user", $cookievalue, time()+3600, "/");
+		UPDATE PERSON (active_cookie) VALUES ($cookievalue) WHERE username = $uname; 
+	}else{
+		echo 'error!';
+	}
+} 
+/*sanitize this for sql injection, also we must add a connection to the database at the beginning once we have a database.*/
+?>
 </body>
 </html>
