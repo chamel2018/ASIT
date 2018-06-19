1dead1618287affa98be0f7864944ff97814dba2
JackDenson <jdenson2019@student.amsacs.org>
2018-06-19 12:43:53 -0400

    Final code push

--- htdocs/php/NewQuestion.php
+++ htdocs/php/NewQuestion.php
@@ -1,6 +1,16 @@
 <?php 
 	if(isset($_POST['submit'])){
 		$newQuestion = $_POST['newQuestionName']
+		
+		$servername = "localhost:8080";
+		$username = "root";
+		$password = "";
+		$conn = new mysqli($servername, $username, $password);
+		if (!$conn) {
+  			echo "Connection Failed"
+		}
+		/*this connects to the database*/
+
 		mysql_query("INSERT INTO QUESTION(question_id, category_id, input_type_id, statement) VALUES((SELECT 1+MAX(question_id) FROM QUESTION), 1, 1, ".$newQuestion.")")
 	}
 	<script type="text/javascript">
